a643792d1f80376f611e5f52809d977b65c7814f
oldtimerza <bradley.morris@crvsh.io>
2019-09-27 23:28:35 +0200

    Added svelte app
    Added simple svelte app with broserify bundling

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const Mousetrap = require('./mousetrap.min.js');
-// single keys
-Mousetrap.bind('4', () => { console.log('4'); });
-Mousetrap.bind('?', () => { console.log('show shortcuts!'); });
-Mousetrap.bind('esc', () => { console.log('escape'); }, 'keyup');
+const App = require('./app.svelte');
 
-// combinations
-Mousetrap.bind('command+shift+k', () => { console.log('command shift k'); });
-
-// map multiple combinations to the same callback
-Mousetrap.bind(['command+k', 'ctrl+k'], () => {
-  console.log('command k or control k');
-
-  // return false to prevent default browser behavior
-  // and stop event from bubbling
-  return false;
+const app = new App({
+  target: document.body,
 });
 
-// gmail style sequences
-Mousetrap.bind('g i', () => { console.log('go to inbox'); });
-Mousetrap.bind('* a', () => { console.log('select all'); });
-
-// konami code!
-Mousetrap.bind('up up down down left right left right b a enter', () => {
-  console.log('konami code');
-});
+module.exports = app;
---
+++ app.svelte
@@ -0,0 +1,30 @@
+<script>
+    const Mousetrap = require('./mousetrap.min.js');
+    // single keys
+    Mousetrap.bind('4', () => { console.log('4'); });
+    Mousetrap.bind('?', () => { console.log('show shortcuts!'); });
+    Mousetrap.bind('esc', () => { console.log('escape'); }, 'keyup');
+
+    // combinations
+    Mousetrap.bind('command+shift+k', () => { console.log('command shift k'); });
+
+    // map multiple combinations to the same callback
+    Mousetrap.bind(['command+k', 'ctrl+k'], () => {
+    console.log('command k or control k');
+
+    // return false to prevent default browser behavior
+    // and stop event from bubbling
+    return false;
+    });
+
+    // gmail style sequences
+    Mousetrap.bind('g i', () => { console.log('go to inbox'); });
+    Mousetrap.bind('* a', () => { console.log('select all'); });
+
+    // konami code!
+    Mousetrap.bind('up up down down left right left right b a enter', () => {
+    console.log('konami code');
+    });
+</script>
+
+<h1>Welcome to meerkat - the simple VIM inspired todo maker</h1>
